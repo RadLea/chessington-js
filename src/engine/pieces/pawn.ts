3224c0daf744b1aa323040dc7c69eb5607d86e92
RadLea <radu.leanca@softwire.com>
2023-06-28 17:28:02 +0300

    made pwans move 2 squares on first move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -10,14 +11,30 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         const pawnPos = board.findPiece(this);
         const movesArr = [];
+        
+        let modifierOne = 0;
+        let modifierTwo = 0;
+
         if (this.player === Player.WHITE) {
-            pawnPos.row++;
+            if (pawnPos.row === 1) {
+              modifierTwo = 2;
+            }
+            modifierOne = 1;
         } else {
-            pawnPos.row--;
+            if (pawnPos.row === 6) {
+                modifierTwo = -2;
+            }
+            modifierOne = -1;
         }
-        if (board.getPiece(pawnPos) === undefined) {
-            movesArr.push(pawnPos);
+
+        const moveOne = new Square(pawnPos.row + modifierOne, pawnPos.col);
+        movesArr.push(moveOne);
+
+        if (modifierTwo !== 0) {
+            const moveTwo = new Square(pawnPos.row + modifierTwo, pawnPos.col);
+            movesArr.push(moveTwo);
         }
+
         return movesArr;
     }
 }
