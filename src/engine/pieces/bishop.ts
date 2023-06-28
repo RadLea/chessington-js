7c24825c2e08ceb0dd98061855b0c3ec39713d89
RadLea <radu.leanca@softwire.com>
2023-06-28 18:45:04 +0300

    made bishop move

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const bishopPos = board.findPiece(this);
+        const movesArr = [];
+
+        for (let i = 1; bishopPos.col - i >= 0 && bishopPos.row - i >= 0; i++) {
+            movesArr.push(new Square(bishopPos.row - i, bishopPos.col - i));
+        }
+
+        for (let i = 1; bishopPos.col + i < 8 && bishopPos.row - i >= 0; i++) {
+            movesArr.push(new Square(bishopPos.row - i, bishopPos.col + i));
+        }
+
+        for (let i = 1; bishopPos.col - i >= 0 && bishopPos.row + i < 8; i++) {
+            movesArr.push(new Square(bishopPos.row + i, bishopPos.col - i));
+        }
+
+        for (let i = 1; bishopPos.col + i < 8 && bishopPos.row + i < 8; i++) {
+            movesArr.push(new Square(bishopPos.row + i, bishopPos.col + i));
+        }
+
+        return movesArr;
     }
 }
